64ababa59781d9dee3a4fe4cab191d45c0c163ed
minakshiaditya <minakshitech08@gmail.com>
2024-08-12 11:33:14 +0530

    first comment

---
+++ List.py
@@ -0,0 +1,49 @@
+1.	#How do you create a list in Python?
+List=[1,2,3]
+print(List)
+
+2.	#How can you add an element to the end of a list?
+List=[1,2,3]
+List.append(4)
+print(List)
+
+3.	#How do you remove an element from a list by its value?
+List=[1,2,3]
+List.remove(2)
+print(List)
+
+4.#How can you access the third element in a list?
+List=[1,2,3,4]
+print(List[3])
+
+5.	#Write a Python program to find the largest number in a list.
+List=[1, 5, 3, 9, 2]
+print(List[3])
+    
+6.#Write a Python program to remove duplicates from a list.
+List=[1, 2, 2, 3, 4, 4, 5]
+List.remove(2)
+print(List)
+List.remove(4)
+print(List)
+
+7.	#How do you iterate over each element in a list?
+List=[1, 2, 3]
+for i in List:
+    print(List)
+    
+8.	#Write a Python program to count the number of occurrences of an element in a list.
+list =[1, 2, 2, 3, 2, 4, 2]
+count=list.count(2)
+print(count)
+
+9.	#How do you sort a list in ascending order?
+list =[3, 1, 4, 1, 5, 9, 2, 6] 
+print(list.sort())
+print(list)
+
+10.	#Write a Python program to merge two lists into a single list.
+list1 =[1, 2, 3] 
+list2=[4, 5, 6]
+
+
